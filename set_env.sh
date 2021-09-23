b9e632fc9a0b4bba206843f8d531580bf7028166
rrprajiv <88769287+rrprajiv@users.noreply.github.com>
2021-09-23 15:06:24 +0530

    Initial Commit

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramrajiv
+export POSTGRES_PASSWORD=udaGRAMRAJiv
+export POSTGRES_DB=udagramdb
+export POSTGRES_HOST=udagramrajivinstance.csckvju1v5x0.us-east-2.rds.amazonaws.com
+#export AWS_BUCKET=arn:aws:s3:::udagram-rajivpandey-dev
+export AWS_BUCKET=udagram-rajivpandey-dev
+export AWS_MEDIA_BUCKET=udagram-rajivpandey-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
